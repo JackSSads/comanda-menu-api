ebe7ad311521586fdde8323e7cbebf3ff3f66910
JackSSads <fotosjackson2018@hotmail.com>
2023-12-06 13:33:37 -0300

    Configuração de acessos

--- app.js
+++ app.js
@@ -19,6 +19,15 @@ app.use(express.json());
 
 app.use(cors());
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  
+    next();
+  });
+
 app.use("/login", loginRouter);
 app.use("/usuario", userRouter);
 app.use("/comanda", comandaRoutes);
