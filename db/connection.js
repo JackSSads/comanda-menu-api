884d5ffdca3a316bb78c8d3f07140adc07985254
JackSSads <fotosjackson2018@hotmail.com>
2023-12-07 16:21:35 -0300

    passando conexão do banco com dovenv

--- db/connection.js
+++ db/connection.js
@@ -2,6 +2,6 @@ require("dotenv").config();
 
 const mongoose = require("mongoose");
 
-const connection = mongoose.connect(process.env.MONGO_URL);
+const connection = mongoose.connect(process.env.MONGO_ATLAS);
 
 module.exports = connection;
